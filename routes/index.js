11190917b04dd6fd4598b15c043c0281825edf54
oezhasan <hasan98oez@googlemail.com>
2022-03-22 20:57:53 +0100

    implementatio finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Message Board" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+
+  res.redirect("/"); // send user back to index page after submitting the form
 });
 
 module.exports = router;
